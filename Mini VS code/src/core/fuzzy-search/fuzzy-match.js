44e297ecab5f24c71d7ac605c522faa44aa24370
Nawin chander <nawinnapzy289@gmail.com>
2026-05-23 01:29:12 +0530

    add

---
+++ Mini VS code/src/core/fuzzy-search/fuzzy-match.js
@@ -0,0 +1,19 @@
+export function fuzzyMatch(
+  input,
+  target
+) {
+
+  input = input.toLowerCase();
+  target = target.toLowerCase();
+
+  let i = 0;
+
+  for (const char of target) {
+
+    if (char === input[i]) {
+      i++;
+    }
+  }
+
+  return i === input.length;
+}
