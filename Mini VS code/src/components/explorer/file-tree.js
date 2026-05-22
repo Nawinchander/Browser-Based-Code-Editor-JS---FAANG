bf3416eb13ad49c501730102136e65f561cae3f3
Nawin chander <nawinnapzy289@gmail.com>
2026-05-22 12:53:50 +0530

    add

---
+++ Mini VS code/src/components/explorer/file-tree.js
@@ -0,0 +1,29 @@
+export const files = [
+  {
+    id: '1',
+    name: 'src',
+    type: 'folder',
+
+    children: [
+      {
+        id: '2',
+        name: 'App.js',
+        type: 'file'
+      },
+
+      {
+        id: '3',
+        name: 'components',
+        type: 'folder',
+
+        children: [
+          {
+            id: '4',
+            name: 'Editor.js',
+            type: 'file'
+          }
+        ]
+      }
+    ]
+  }
+];
