804ee8835a908a399e8a2bb6cf04172ecb8de857
Nawin chander <nawinnapzy289@gmail.com>
2026-05-22 12:48:11 +0530

    add

---
+++ Mini VS code/src/core/editor/event-bus.js
@@ -0,0 +1,24 @@
+class EventBus {
+
+  constructor() {
+    this.events = {};
+  }
+
+  on(event, callback) {
+
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+
+    this.events[event].push(callback);
+  }
+
+  emit(event, data) {
+
+    if (!this.events[event]) return;
+
+    this.events[event].forEach(cb => cb(data));
+  }
+}
+
+export const bus = new EventBus();
