811595c14c467ed72dd39762da15d1aa83a4bf75
Nawin chander <nawinnapzy289@gmail.com>
2026-05-23 01:26:24 +0530

    add

---
+++ Mini VS code/src/services/command-registry.js
@@ -0,0 +1,26 @@
+export const commands = [
+
+  {
+    id: '1',
+    name: 'Open File',
+    action: () => {
+      console.log('Open file');
+    }
+  },
+
+  {
+    id: '2',
+    name: 'Toggle Theme',
+    action: () => {
+      console.log('Theme changed');
+    }
+  },
+
+  {
+    id: '3',
+    name: 'Close Editor',
+    action: () => {
+      console.log('Editor closed');
+    }
+  }
+];
