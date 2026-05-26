5e3ee4dabfbe9a3518b6265f3be1b448d38c7f3a
Nawin chander <nawinnapzy289@gmail.com>
2026-05-26 13:49:08 +0530

    add

--- Mini VS code/src/services/indexeddb-service.js
+++ Mini VS code/src/services/indexeddb-service.js
@@ -26,3 +26,40 @@ export function openDB() {
     };
   });
 }
+
+
+export async function saveFile(
+  file
+) {
+
+  const db = await openDB();
+
+  const tx =
+    db.transaction(
+      STORE_NAME,
+      'readwrite'
+    );
+
+  tx.objectStore(STORE_NAME)
+    .put(file);
+}
+
+
+export async function getFile(id) {
+
+  const db = await openDB();
+
+  return new Promise(resolve => {
+
+    const tx =
+      db.transaction(STORE_NAME);
+
+    const request =
+      tx.objectStore(STORE_NAME)
+      .get(id);
+
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+  });
+}
